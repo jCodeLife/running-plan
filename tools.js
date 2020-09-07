3150f05f0c2a5c7b8bb625e5b9c0582508298a23
lbj <bingjin.liao@outlook.com>
2020-09-07 12:22:37 +0800

    手动封装ajax

--- tools.js
+++ tools.js
@@ -98,10 +98,66 @@
 // }
 
 
- // 封装一个兼容所有浏览器的方法，用于查看滚动条滚动距离：
-    // window.getScrollLT = function () {
-    //   return {
-    //     left: window.pageXOffset || document.documentElement.scrollLeft + document.body.scrollLeft,
-    //     top: window.pageYOffset || document.documentElement.scrollTop + document.body.scrollTo
-    //   }
-    // }
+// 封装一个兼容所有浏览器的方法，用于查看滚动条滚动距离：
+// window.getScrollLT = function () {
+//   return {
+//     left: window.pageXOffset || document.documentElement.scrollLeft + document.body.scrollLeft,
+//     top: window.pageYOffset || document.documentElement.scrollTop + document.body.scrollTo
+//   }
+// }
+
+
+//  模拟jQuery中的ajax，手动封装一个自己的ajax()
+// function ajax(option) {//type,url,data,timeout,success,error将所有参数换成一个对象{}
+//    //  0.将对象转换成字符串
+//    var str = objToString(option.data);
+//    //  1.创建一个异步对象xmlhttp；
+//    var xmlhttp, timer;
+//    if (window.XMLHttpRequest) {
+//       xmlhttp = new XMLHttpRequest();
+//    } else {// code for IE6, IE5 
+//       xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//    }
+//    //  2.设置请求方式和请求地址； 
+//    // 判断请求的类型是POST还是GET
+//    if (option.type.toLowerCase() === 'get') {
+//       xmlhttp.open(option.type, option.url + "?t=" + str, true);
+//       //  3.发送请求；
+//       xmlhttp.send();
+//    } else {
+//       xmlhttp.open(option.type, option.url, true);
+//       // 注意：在post请求中，必须在open和send之间添加HTTP请求头：setRequestHeader(header,value);
+//       xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//       //  3.发送请求；
+//       xmlhttp.send(str);
+//    }
+//    //  4.监听状态的变化；
+//    xmlhttp.onreadystatechange = function () {
+//       clearInterval(timer);
+//       if (xmlhttp.readyState === 4) {
+//          if (xmlhttp.status >= 200 && xmlhttp.status < 300 || xmlhttp.status == 304) {
+//             //  5.处理返回的结果；
+//             option.success(xmlhttp);//成功后回调；
+//          } else {
+//             option.error(xmlhttp);//失败后回调；
+//          }
+//       }
+//    }
+//    //处理obj 
+//    function objToString(data) {
+//       data.t = new Date().getTime();
+//       var res = [];
+//       for (var key in data) {
+//          //需要将key和value转成非中文的形式，因为url不能有中文。使用encodeURIComponent();
+//          res.push(encodeURIConponent(key) + " = " + encodeURIConponent(data[key]));
+//       }
+//       return res.join("&");
+//    }
+//    //判断外界是否传入了超时时间
+//    if (timeout) {
+//       timer = setInterval(function () {
+//          xmlhttp.abort();//中断请求
+//          clearInterval(timer);
+//       }, timeout);
+//    }
+// }
