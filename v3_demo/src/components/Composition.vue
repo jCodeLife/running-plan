d0769b91a41439141866f795994f53bb80a2600e
lbj <bingjin.liao@outlook.com>
2020-09-28 11:06:57 +0800

    reactive的基本使用

--- v3_demo/src/components/Composition.vue
+++ v3_demo/src/components/Composition.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
-    <h1>{{ age }}</h1>
-    <button @click="change_age">点我</button>
+    <h1>{{ count }}</h1>
+    <button @click="change_count">点我</button>
+    <h1>{{ alice.name }}</h1>
+    <h1>{{ alice.age }}</h1>
+    <button @click="change_obj">change Object</button>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue';
+import {ref,reactive} from 'vue';
 export default {
   name: "Composition",
   setup(){
-    let age = ref(18);
-    function change_age(){
-      age.value +=1;
-      console.log(age)
+    let count = ref(0);
+    function change_count(){
+      count.value +=1;
     }
-    return{age, change_age}
+    const obj = {
+      name:'Alice',
+      age:18
+    }
+    const alice = reactive(obj);
+    function change_obj(){
+      alice.name = 'tom'
+      alice.age = 20
+    }
+    return{count, change_count,alice,change_obj}
   }
 };
 </script>
