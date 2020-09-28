5fe4d578c36ef73b968d04c3a57823df4f8ba01f
lbj <bingjin.liao@outlook.com>
2020-09-28 10:51:09 +0800

    composition中ref函数的基本使用

---
+++ v3_demo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Composition/>
+</template>
+
+<script>
+import Composition from './components/Composition.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Composition
+  }
+}
+</script>
---
+++ v3_demo/src/components/Composition.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ age }}</h1>
+    <button @click="change_age">点我</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue';
+export default {
+  name: "Composition",
+  setup(){
+    let age = ref(18);
+    function change_age(){
+      age.value +=1;
+      console.log(age)
+    }
+    return{age, change_age}
+  }
+};
+</script>
