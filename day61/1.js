41097719354b314f16a0e63df36023fb4c246a07
lbj <bingjin.liao@outlook.com>
2020-10-18 23:08:54 +0800

    ts基本数据类型

---
+++ day61/1.js
@@ -0,0 +1,13 @@
+var a = 5;
+var str = 'hello ts';
+var isDone = true;
+var n1 = null;
+var u1 = undefined;
+// -------------------------------
+var n2 = undefined;
+var u2 = null;
+function fn() {
+    console.log('This is a fn,but return is void');
+}
+var any1 = 'xxx';
+var obj = {};
---
+++ day61/1.ts
@@ -0,0 +1,19 @@
+let a: number = 5
+
+let str: string = 'hello ts'
+
+let isDone: boolean = true
+
+let n1: null = null
+let u1: undefined = undefined
+// -------------------------------
+let n2: null = undefined
+let u2: undefined = null
+
+function fn(): void {
+    console.log('This is a fn,but return is void')
+}
+
+let any1: any = 'xxx'
+
+let obj:object = {}
